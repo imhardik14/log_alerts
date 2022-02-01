86301d00cf1c7d0c5e71f375af861922c631e004
imhardik14 <98335065+imhardik14@users.noreply.github.com>
2022-02-01 20:41:58 +0000

    initlal commit

---
+++ src/main/java/com/hardik4u/log_alerts/LogAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.hardik4u.log_alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAlertsApplication.class, args);
+	}
+
+}
