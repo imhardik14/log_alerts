3cf34f759a26e4e70c0016e830d54e9817ee0069
imhardik14 <98335065+imhardik14@users.noreply.github.com>
2022-02-02 18:45:26 +0000

    Add files via upload
    file changes

--- src/main/java/com/hardik4u/log_alerts/LogAlertsApplication.java
+++ src/main/java/com/hardik4u/log_alerts/LogAlertsApplication.java
@@ -1,13 +1,27 @@
 package com.hardik4u.log_alerts;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LogAlertsApplication {
+public class LogAlertsApplication implements CommandLineRunner {
 
+	
 	public static void main(String[] args) {
+		
+		for(String arg:args)
+			System.out.println(arg);
+		
 		SpringApplication.run(LogAlertsApplication.class, args);
+		
+		
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Thread.currentThread().join();
+	}
+
+	
 }
